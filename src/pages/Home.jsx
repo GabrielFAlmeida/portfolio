3cfda3bfbad1239ac1292db622248b8752fdc534
Gabriel França de Almeida <54248551+GabrielFAlmeida@users.noreply.github.com>
2025-05-09 01:16:43 -0300

    ! GF-001 build: adicionar postcss e configurar arquivos - primeira versão da home
    Refs: ligada a branch #GF-001

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import OndulateImage from "../assets/ondulation.svg?react";
+
 export default function Home() {
   return (
-    <div className="">
-        <p>Teste</p>
+    <div className="w-screen h-screen bg-main-blue-100 bg-ondulation bg-[size:80%] bg-position-[-160%_40%] bg-no-repeat">
+      {/* <OndulateImage className="w-2/3"/> */}
     </div>
   );
 }
