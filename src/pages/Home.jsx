690c0484a202fe64765794a30e0a340140aeabbc
Gabriel França de Almeida <54248551+GabrielFAlmeida@users.noreply.github.com>
2025-05-11 00:04:37 -0300

    GF-001 style(Home): melhorar estilo do plano de fundo e dos textos
    Refs: ligada a task #GF-001

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
-import OndulateImage from "../assets/ondulation.svg?react";
+import Memoji from "../assets/svg/memoji.svg?react";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-main-blue-100 bg-ondulation bg-[size:80%] bg-position-[-160%_40%] bg-no-repeat">
-      {/* <OndulateImage className="w-2/3"/> */}
+    <div className="w-screen h-screen flex flex-row justify-center items-center bg-main-blue-100 bg-ondulation bg-[size:80%] bg-position-[-160%_40%] bg-no-repeat bg-blend-hard-light">
+      <div>
+        <h1 className="text-[calc(100vw*0.06)]">Título</h1>
+        <h2 className="text-[calc(100vw*0.03)]">Subtítulo</h2>
+      </div>
+      <div className="flex justify-center items-center size-1/2vw rounded-full bg-main-blue-500 md:size-1/2vh">
+         <Memoji/>
+      </div>
     </div>
   );
 }
