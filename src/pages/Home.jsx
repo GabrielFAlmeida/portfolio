542cc6248da287815a7f291463ec309bc4de0eab
Gabriel França de Almeida <54248551+GabrielFAlmeida@users.noreply.github.com>
2025-05-11 16:41:40 -0300

    GF-001 style(Home): melhorando estilos para desktop
    Mobile ainda precisa ser otimizado
    
    Refs: ligada a task #GF-001

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,19 @@ import Memoji from "../assets/svg/memoji.svg?react";
 export default function Home() {
   return (
     <div className="w-screen h-screen flex flex-row justify-center items-center bg-main-blue-100 bg-ondulation bg-[size:80%] bg-position-[-160%_40%] bg-no-repeat bg-blend-hard-light">
-      <div>
-        <h1 className="text-[calc(100vw*0.06)]">Título</h1>
-        <h2 className="text-[calc(100vw*0.03)]">Subtítulo</h2>
-      </div>
-      <div className="flex justify-center items-center size-1/2vw rounded-full bg-main-blue-500 md:size-1/2vh">
-         <Memoji/>
+      <div className="flex flex-col justify-center md:flex-row md:items-center md:max-w-3/4">
+        <div className="flex justify-center items-center mb-4 p-4 rounded-full bg-main-blue-500 md:min-w-1/2vh md:mr-8">
+          <Memoji />
+        </div>
+        <div>
+          <h1 className="text-5.25xl font-poppins mb-6">
+            Oi, sou o Gabriel França ;)
+          </h1>
+          <h2 className="text-2xl font-poppins">
+            Desenvolvedor full stack, brasileiro, apaixonado por web,
+            mergulhando no universo da inteligência artificial
+          </h2>
+        </div>
       </div>
     </div>
   );
