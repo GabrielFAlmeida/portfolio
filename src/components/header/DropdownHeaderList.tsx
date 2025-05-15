ab433de8295aaeabb563483256de42b578083977
Gabriel França de Almeida <54248551+GabrielFAlmeida@users.noreply.github.com>
2025-05-15 01:03:09 -0300

    GF-002 style(Header): melhorar estilo e organização para o mobile
    Refs: ligada a task #GF-002

--- src/components/header/DropdownHeaderList.tsx
+++ src/components/header/DropdownHeaderList.tsx
@@ -5,7 +5,7 @@ export default function DropdownHeaderList() {
     <nav
       id="dropdown-header-list"
       popover="auto"
-      className="w-full absolute top-28 p-4 rounded-2xl bg-transparent backdrop-blur-xl border-2 text-main-blue-500 md:hidden"
+      className="w-45% absolute top-20 left-auto animate-fade-in mr-14 p-4 rounded-2xl bg-transparent backdrop-blur-xl border-2 text-main-blue-500 sm:w-1/4 md:hidden"
     >
       <ul className="flex flex-col justify-center items-center gap-2">
         <HeaderListItem />
